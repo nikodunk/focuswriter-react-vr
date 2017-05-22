6b781cf734acec5100ab77e037d684d92369ca2c
Nikolai <nikolaidunkel@Nikolais-MBP.lan>
2017-05-22 14:03:19 -0400

    Updates

--- client.js
+++ client.js
@@ -6,7 +6,7 @@
 import {VRInstance} from 'react-vr-web';
 
 function init(bundle, parent, options) {
-  const vr = new VRInstance(bundle, 'Focus', parent, {
+  const vr = new VRInstance(bundle, 'Main', parent, {
     // Add custom options here
     ...options,
   });
